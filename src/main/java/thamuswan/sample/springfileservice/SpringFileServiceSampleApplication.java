5a597276164a72ebe8155e093f3f7928ebdfec7a
Thanawat Muangsawang <thanawat.muangsawang@gmail.com>
2022-07-10 19:43:56 +0700

    Initial commit

---
+++ src/main/java/thamuswan/sample/springfileservice/SpringFileServiceSampleApplication.java
@@ -0,0 +1,13 @@
+package thamuswan.sample.springfileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileServiceSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFileServiceSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thamuswan/sample/springfileservice/SpringFileServiceSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package thamuswan.sample.springfileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileServiceSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
